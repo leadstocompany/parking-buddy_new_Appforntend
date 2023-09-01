0dc66790f5e09b0b34400079e58457927fa7ce0b
ABHISHEK Mehra <abhishekmehra1245@gmail.com>
2023-09-01 14:44:32 +0530

    basic structure completed

---
+++ src/app/customers/paymentpage/paymentpage.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-paymentpage',
+  templateUrl: './paymentpage.component.html',
+  styleUrls: ['./paymentpage.component.scss']
+})
+export class PaymentpageComponent {
+  step = 0;
+
+  setStep(index: number) {
+    this.step = index;
+  }
+
+  nextStep() {
+    this.step++;
+  }
+
+  prevStep() {
+    this.step--;
+  }
+}
